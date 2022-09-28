bfb593ab48dd4a39ad156132e55124c2b280f2b5
okhentai <kyakyadive@gmail.com>
2022-09-28 11:12:42 +0900

    Anh xa trong Gen act

--- app/src/main/java/com/example/qrhomnay/GenerateQRCode.java
+++ app/src/main/java/com/example/qrhomnay/GenerateQRCode.java
@@ -3,12 +3,27 @@ package com.example.qrhomnay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class GenerateQRCode extends AppCompatActivity {
-
+    ImageView iv_QRCode;
+    EditText et_Input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_qrcode);
+
+        AddControls();
+        AddEvents();
+    }
+
+    private void AddEvents() {
+
+    }
+
+    private void AddControls() {
+        iv_QRCode = findViewById(R.id.iv_qr_code);
+        et_Input = findViewById(R.id.et_input);
     }
 }
