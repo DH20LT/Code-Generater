20930821abeb7b7a6a1d45a36bd3d2cf4c84eb2e
okhentai <kyakyadive@gmail.com>
2022-09-28 10:08:08 +0900

    add controls and events

--- app/src/main/java/com/example/qrhomnay/MainActivity.java
+++ app/src/main/java/com/example/qrhomnay/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.qrhomnay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+adimport android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button QuetQR;
+    Button TaoQR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AddControls();
+        AddEvents();
     }
+
+    private void AddEvents() {
+        QuetQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        TaoQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    // Dung de anh xa
+    private void AddControls() {
+        QuetQR = findViewById(R.id.quet_qr);
+        TaoQR = findViewById(R.id.tao_qr);
+    }
+
+
 }
